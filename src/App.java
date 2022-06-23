0bf126df863b4db9acbfc83e7d64cb48a8cda191
cmdgeneration <resnonverba93@gmail.com>
2022-06-23 19:30:19 +0200

    Se agrega el segundo proyecto

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("¡Bienvenido al conversor de moneda!");
+        System.out.println("Introduzca una cantidad en euros y el programa le devolverá la conversión en US dólares: ");
+        
+        Scanner myObjScanner = new Scanner(System.in);
+        double euros = myObjScanner.nextDouble();
+
+        double equivalencia = 1/0.947771;
+
+        System.out.println("La cantidad de " + euros + " euros equivalen a " + (euros*equivalencia) + " US dólares");
+    }
+}
